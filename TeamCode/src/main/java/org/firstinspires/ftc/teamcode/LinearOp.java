a8c61a6607afdfc0526702a18db096f383cbddbb
BMA <bruceatknson01@gmail.com>
2019-12-17 19:52:07 -0700

    12/17/19
    First upload

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/LinearOp.java
@@ -50,8 +50,8 @@ import com.qualcomm.robotcore.util.Range;
  * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
  */
 
-@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
-@Disabled
+@TeleOp(name="LinearOp", group="Linear Opmode")
+//@Disabled
 public class LinearOp extends LinearOpMode {
 
     // Declare OpMode members. Hello world!
